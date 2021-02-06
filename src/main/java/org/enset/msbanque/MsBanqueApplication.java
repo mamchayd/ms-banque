4f1250a68bf3a0301a4ba54b178813ee544bfd37
Amchayd Mariam <meriam.amchayd@gmail.com>
2021-02-06 18:17:49 +0100

    Initial commit

---
+++ src/main/java/org/enset/msbanque/MsBanqueApplication.java
@@ -0,0 +1,13 @@
+package org.enset.msbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBanqueApplication.class, args);
+    }
+
+}
